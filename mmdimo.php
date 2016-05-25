4d2820b02eb91dbde4b0d5fb24fcc3521358bf5e
danillonunes <danillonunes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-05-25 23:38:37 +0000

    Tagging 1.4 version
    git-svn-id: https://plugins.svn.wordpress.org/makemydonation-imo/trunk@1424408 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mmdimo.php
+++ mmdimo.php
@@ -6,7 +6,7 @@
 Plugin Name: Make My Donation – In Memory Of
 Plugin URI: https://wordpress.org/plugins/makemydonation-imo
 Description: Make My Donation – In Memory Of Plugin.
-Version: 1.3
+Version: 1.4
 Author: Make My Donation
 Author URI: http://makemydonation.org
 License: GPLv2 or later
