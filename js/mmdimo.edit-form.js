82919fe34f042a45ad45d3bfab6b71a0cce5e62b
danillonunes <danillonunes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-11-06 21:33:51 +0000

    Replace charity selector with OrgHunter CSC
    git-svn-id: https://plugins.svn.wordpress.org/makemydonation-imo/trunk@1970115 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/mmdimo.edit-form.js
+++ js/mmdimo.edit-form.js
@@ -50,19 +50,43 @@ $('#mmdimo_meta_box').each(function() {
   var metabox = this;
   var $create = $('#mmdimo-case-create', metabox);
   var $data = $('.case-data', metabox);
-  var $state = $('#mmdimo-charity-state', metabox);
   var $charities = $('#mmdimo-charities', metabox);
+  var $orghunterCSC = $('#orghunter-csc', metabox);
   var $select = $('#mmdimo-charity-select input', metabox);
   var $all = $('#mmdimo-charity-select-all', metabox);
   var $single = $('#mmdimo-charity-select-select', metabox);
   var $charitySelect = $('#mmdimo-charity-selection', metabox);
   var $charityMeta = $('#mmdimo-charity-metadata', metabox);
-  var $charityName = $charities.after('<input type="text" id="mmdimo-charity-name" size="40">').next('#mmdimo-charity-name');
-  var $charityActive = $state.before('<ul id="mmdimo-charity-active" class="mmdimo-empty"></ul>').prev('#mmdimo-charity-active');
+  var $charityActive = $orghunterCSC.before('<ul id="mmdimo-charity-active" class="mmdimo-empty"></ul>').prev('#mmdimo-charity-active');
   var metadata = {};
-  var options, engine, dataset;
 
-  if ($charities.length && $charityName.length) {
+  if ($charities.length) {
+    $orghunterCSC.orghunterCSC({
+      apiURL: ajaxurl,
+      apiData: {
+        action: 'mmdimo_orghunter_csc_ajax',
+      },
+      searchAlias: true,
+      size: 'small',
+      defaultState: $orghunterCSC.attr('data-default-state'),
+      onCharitySelect: function(ein, data) {
+        var charity = {
+          ein: ein,
+          ein_hash: data.ein_hash,
+          parent_ein: data.parent_ein,
+          name: data.charityName,
+          url: data.url,
+          city: data.city,
+          state: data.state
+        };
+        $charities.trigger('mmdimo-add', [charity]);
+        $orghunterCSC.trigger('ohcsc-reset', [{
+          keepState: true,
+          keepCity: true,
+        }]);
+      }
+    });
+
     $charities
       .hide()
       .on('mmdimo-add', function(e, charity) {
@@ -94,86 +118,6 @@ $('#mmdimo_meta_box').each(function() {
       .on('mmdimo-reval', function() {
         $charities.val(Object.keys(metadata).join(',').replace(/charity-ein-/g, ''));
         $charityMeta.val(JSON.stringify(metadata));
-      });
-
-    $charityName
-      .attr('autocomplete', 'OFF')
-      .attr('aria-autocomplete', 'list');
-
-    $state
-      .bind('change', function() {
-        var st = $(this).val();
-        var state = '';
-
-        if (st) {
-          state = $state.find('option[value="' + $(this).val() + '"]').text() + ' ';
-        }
-
-        $charityName.attr('placeholder', 'Search all ' + state + 'charities');
-      })
-      .trigger('change');
-
-    engine = new Bloodhound({
-      identify: function(o) { return o.id_str; },
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace(),
-      dupDetector: function(a, b) { return a.id_str === b.id_str; },
-      remote: {
-        url: 'https://funerals.makemydonation.org/orghunter/charitysearchaliasall/autocomplete/%QUERY/typeahead?eligible=1',
-        prepare: function(query, settings) {
-          var state = $state.val();
-          settings.url = settings.url.replace('%QUERY', encodeURIComponent(query));
-          if (state) {
-            settings.url = settings.url + '&state=' + state;
-          }
-          return settings;
-        },
-        transform: function(results) {
-          var arr = [];
-
-          for (var key in results) {
-            if (results.hasOwnProperty(key)) {
-              arr.push(results[key]);
-            }
-          }
-
-          arr.sort(function(a, b) {
-            if (a.weight && b.weight) {
-              if (a.weight < b.weight) {
-                return -1;
-              }
-              if (a.weight > b.weight) {
-                return 1;
-              }
-            }
-            return 0;
-          });
-
-          return arr;
-        },
-      },
-    });
-    options = {};
-    dataset = {
-      source: engine,
-      display: function(suggestion) {
-        return suggestion.name;
-      },
-      templates: {
-        suggestion: function(suggestion) {
-          return '<div><strong>' + suggestion.name + '</strong><br><span>EIN: ' + suggestion.ein + ' — ' + suggestion.city + ', ' + suggestion.state + '</span></div>';
-        }
-      },
-    };
-
-    $charityName
-      .typeahead(options, dataset)
-      .bind('typeahead:select', function(event, suggestion) {
-        $charities.trigger('mmdimo-add', suggestion);
-
-        $charityName
-          .typeahead('close')
-          .typeahead('val', '');
       });
 
     if ($charityMeta.val()) {
--- metabox.php
+++ metabox.php
@@ -39,60 +39,7 @@
       </div>
 
       <div id="mmdimo-charity-selection">
-        <select id="mmdimo-charity-state" name="mmdimo_charity_state">
-          <option value=""><?php _e('All states', 'mmdimo'); ?></option>
-          <option value="AL" <?php if ('AL' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Alabama', 'mmdimo'); ?></option>
-          <option value="AK" <?php if ('AK' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Alaska', 'mmdimo'); ?></option>
-          <option value="AZ" <?php if ('AZ' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Arizona', 'mmdimo'); ?></option>
-          <option value="AR" <?php if ('AR' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Arkansas', 'mmdimo'); ?></option>
-          <option value="CA" <?php if ('CA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('California', 'mmdimo'); ?></option>
-          <option value="CO" <?php if ('CO' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Colorado', 'mmdimo'); ?></option>
-          <option value="CT" <?php if ('CT' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Connecticut', 'mmdimo'); ?></option>
-          <option value="DE" <?php if ('DE' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Delaware', 'mmdimo'); ?></option>
-          <option value="DC" <?php if ('DC' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('District of Columbia', 'mmdimo'); ?></option>
-          <option value="FL" <?php if ('FL' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Florida', 'mmdimo'); ?></option>
-          <option value="GA" <?php if ('GA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Georgia', 'mmdimo'); ?></option>
-          <option value="HI" <?php if ('HI' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Hawaii', 'mmdimo'); ?></option>
-          <option value="ID" <?php if ('ID' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Idaho', 'mmdimo'); ?></option>
-          <option value="IL" <?php if ('IL' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Illinois', 'mmdimo'); ?></option>
-          <option value="IN" <?php if ('IN' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Indiana', 'mmdimo'); ?></option>
-          <option value="IA" <?php if ('IA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Iowa', 'mmdimo'); ?></option>
-          <option value="KS" <?php if ('KS' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Kansas', 'mmdimo'); ?></option>
-          <option value="KY" <?php if ('KY' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Kentucky', 'mmdimo'); ?></option>
-          <option value="LA" <?php if ('LA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Louisiana', 'mmdimo'); ?></option>
-          <option value="ME" <?php if ('ME' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Maine', 'mmdimo'); ?></option>
-          <option value="MD" <?php if ('MD' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Maryland', 'mmdimo'); ?></option>
-          <option value="MA" <?php if ('MA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Massachusetts', 'mmdimo'); ?></option>
-          <option value="MI" <?php if ('MI' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Michigan', 'mmdimo'); ?></option>
-          <option value="MN" <?php if ('MN' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Minnesota', 'mmdimo'); ?></option>
-          <option value="MS" <?php if ('MS' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Mississippi', 'mmdimo'); ?></option>
-          <option value="MO" <?php if ('MO' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Missouri', 'mmdimo'); ?></option>
-          <option value="MT" <?php if ('MT' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Montana', 'mmdimo'); ?></option>
-          <option value="NE" <?php if ('NE' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Nebraska', 'mmdimo'); ?></option>
-          <option value="NV" <?php if ('NV' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Nevada', 'mmdimo'); ?></option>
-          <option value="NH" <?php if ('NH' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('New Hampshire', 'mmdimo'); ?></option>
-          <option value="NJ" <?php if ('NJ' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('New Jersey', 'mmdimo'); ?></option>
-          <option value="NM" <?php if ('NM' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('New Mexico', 'mmdimo'); ?></option>
-          <option value="NY" <?php if ('NY' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('New York', 'mmdimo'); ?></option>
-          <option value="NC" <?php if ('NC' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('North Carolina', 'mmdimo'); ?></option>
-          <option value="ND" <?php if ('ND' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('North Dakota', 'mmdimo'); ?></option>
-          <option value="OH" <?php if ('OH' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Ohio', 'mmdimo'); ?></option>
-          <option value="OK" <?php if ('OK' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Oklahoma', 'mmdimo'); ?></option>
-          <option value="OR" <?php if ('OR' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Oregon', 'mmdimo'); ?></option>
-          <option value="PA" <?php if ('PA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Pennsylvania', 'mmdimo'); ?></option>
-          <option value="RI" <?php if ('RI' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Rhode Island', 'mmdimo'); ?></option>
-          <option value="SC" <?php if ('SC' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('South Carolina', 'mmdimo'); ?></option>
-          <option value="SD" <?php if ('SD' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('South Dakota', 'mmdimo'); ?></option>
-          <option value="TN" <?php if ('TN' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Tennessee', 'mmdimo'); ?></option>
-          <option value="TX" <?php if ('TX' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Texas', 'mmdimo'); ?></option>
-          <option value="UT" <?php if ('UT' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Utah', 'mmdimo'); ?></option>
-          <option value="VT" <?php if ('VT' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Vermont', 'mmdimo'); ?></option>
-          <option value="VA" <?php if ('VA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Virginia', 'mmdimo'); ?></option>
-          <option value="WA" <?php if ('WA' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Washington', 'mmdimo'); ?></option>
-          <option value="WV" <?php if ('WV' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('West Virginia', 'mmdimo'); ?></option>
-          <option value="WI" <?php if ('WI' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Wisconsin', 'mmdimo'); ?></option>
-          <option value="WY" <?php if ('WY' == $mmdimo_default_state) { echo 'selected="selected"'; } ?>><?php _e('Wyoming', 'mmdimo'); ?></option>
-        </select>
+        <div id="orghunter-csc" data-default-state="<?php echo $mmdimo_default_state; ?>"></div>
         <input type="text" id="mmdimo-charities" name="mmdimo_charities" size="20" value="<?php echo $mmdimo_charities; ?>">
         <input type="hidden" id="mmdimo-charity-metadata" name="mmdimo_charity_metadata" value='<?php echo $mmdimo_charity_metadata; ?>'>
         <br>
